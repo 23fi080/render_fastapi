790e8d2fb16d3dbde0c9e0e4d0528476eee25bf7
23fi080 <23fi080@ms.dendai.ac.jp>
2025-06-13 11:58:01 +0900

    kaddai9-1add

--- main.py
+++ main.py
@@ -38,3 +38,18 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+    ### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
